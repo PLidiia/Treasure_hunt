29a9b795951235c9e06cdaf9f19df784765ad608
PLidiia <151846713+PLidiia@users.noreply.github.com>
2023-12-20 11:32:30 +0700

    Обновление констант

--- Constants.py
+++ Constants.py
@@ -1,7 +1,18 @@
+import pygame
+import os
+
+pygame.font.init()
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
+FPS = 60
 DARK_GREEN = (87, 122, 77)
 DARK_RED = (163, 0, 34)
 BROWN = (77, 8, 4)
 BLACK = (0, 0, 0)
-FPS = 60
+BLUE = (65, 179, 222)
+PINK = (255, 208, 214)
+file_font = os.path.join('fonts', 'RubikDoodleShadow-Regular.ttf')
+FONT_38 = pygame.font.Font(file_font, 38)
+FONT_24 = pygame.font.Font(file_font, 24)
+FONT_12 = pygame.font.Font(file_font, 12)
