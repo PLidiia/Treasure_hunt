c9926548adffc53f534842635dcd55b6881b34bc
PLidiia <151846713+PLidiia@users.noreply.github.com>
2023-12-29 16:01:41 +0700

    Работает ввод с записью в базу данных. Простые функции с работой вводом, удаление последнего символа, замена ошибки с пробелом и тд

--- Entrance.py
+++ Entrance.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 import pygame.mouse
 
 from Constants import *
@@ -8,14 +10,36 @@ class Enter:
     def __init__(self):
         self.icon = pygame.image.load(os.path.join('images', 'top_user.png'))
         self.logo_img = pygame.image.load(os.path.join('images', 'logo.png'))
-        self.block_ground = pygame.image.load(os.path.join('images', 'grass.png'))
+        self.alpha = 255
+        self.comments_special = ''
+        self.con = sqlite3.connect('database/users.db')
+        cur = self.con.cursor()
+        cur.execute("""CREATE TABLE IF NOT EXISTS USERS (
+            password TEXT,
+            name TEXT
+        )""")
 
     def draw_items(self, screen):
-        button_1 = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 150, 250, 60, 'Начать вход',
-                          'images/button_image.png', hover_image_path='images/button_image_is_hovered.png')
-        self.button_1 = button_1
-        button_1.draw(screen)
-        return button_1
+        # концепция кнопок не совсем уместна, потому что не обрабатываются для кнопок button_name
+        # и button_db нажатия и кнопки используются только как плашки с текстом
+        button_name = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 150, 250, 60, 'Ввести имя',
+                             'images/button_image.png')
+        button_name.draw(screen)
+        button_db = Button(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - button_name.rect.height + 40
+                           , 250, 60, 'Ввести пароль',
+                           'images/button_image.png')
+        button_db.draw(screen)
+        return [button_name, button_db]
+
+    def screensaver(self, screen):
+        surface_blur = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+        surface_blur.fill((0, 0, 0, self.alpha))
+        screen.blit(surface_blur, (0, 0))
+        self.alpha -= 10
+
+    def update_commenting(self, screen):
+        text = FONT_24.render(self.comments_special, True, WHITE)
+        screen.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, 5))
 
     def run(self):
         pygame.init()
@@ -26,40 +50,77 @@ class Enter:
         pygame.mixer.music.load(os.path.join('music', 'first.mp3'))
         pygame.mixer.music.play()
         enter = Enter()
-        alpha = 255
+        name_intermed_save = ''
         input_text = ''
-        inputting_data = False
         run = True
+        move_to_mail = False
         clock = pygame.time.Clock()
         while run:
             clock.tick(FPS)
+            inputting_data = True
             screen.fill(DARK_GREEN)
-            btn = self.draw_items(screen)
+            buttons = self.draw_items(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                 elif inputting_data and event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
+                    if event.key == pygame.K_RETURN and move_to_mail is True:
+                        cur = self.con.cursor()
+                        search_user = cur.execute('''SELECT name FROM USERS WHERE name = ?''',
+                                                  (name_intermed_save,)).fetchone()
+                        if not search_user:
+                            cur = self.con.cursor()
+                            cur.execute("INSERT INTO USERS (password, name) VALUES (?, ?)",
+                                        (name_intermed_save, input_text))
+                            self.con.commit()
+                            self.comments_special = 'Пользователь с таким именем уже существует'
+                            self.update_commenting(screen)
+                        else:
+                            self.comments_special = 'Пользователь с таким именем уже существует'
+                            self.update_commenting(screen)
+                    elif event.key == pygame.K_RETURN:
                         inputting_data = False
+                        move_to_mail = True
+                        self.comments_special = 'Переключились на введение пароля'
+                        self.update_commenting(screen)
+                        name_intermed_save = input_text
                         input_text = ''
                     elif event.key == pygame.K_BACKSPACE:
-                        pass
+                        input_text = input_text[:-1]
                     else:
                         input_text += event.unicode
-                elif btn.check_click(pygame.mouse.get_pos()):
-                    inputting_data = True
-
+                        self.comments_special = 'Вводятся символы'
+                        self.update_commenting(screen)
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKSPACE:
+                        input_text = input_text[:-1]
+                        self.comments_special = 'Удаляются символы'
+                        self.update_commenting(screen)
+            if len(input_text) > 9 or len(name_intermed_save) > 9:
+                inputting_data = False
+                self.comments_special = 'Слишком длинное поле'
+                self.update_commenting(screen)
+            elif len(input_text) < 9:
+                inputting_data = True
             keys = pygame.key.get_pressed()
             if keys[pygame.K_TAB]:
                 input_text = True
             self.draw_items(screen)
-            self.button_1.check_hover(pygame.mouse.get_pos())
-            self.button_1.draw(screen)
-            if alpha > 0:
-                surface_blur = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
-                surface_blur.fill((0, 0, 0, alpha))
-                screen.blit(surface_blur, (0, 0))
-                alpha -= 4
-            text = FONT_24.render(input_text, True, WHITE)
-            screen.blit(text, (SCREEN_WIDTH // 2 - 180, SCREEN_HEIGHT // 2 - 135))
+            buttons[0].draw(screen)
+            buttons[1].draw(screen)
+            if self.alpha > 0:
+                self.screensaver(screen)
+            if not move_to_mail:
+                text = FONT_24.render(input_text, True, WHITE)
+                screen.blit(text, (SCREEN_WIDTH // 2 - 180, SCREEN_HEIGHT // 2 - 135))
+            else:
+                text = FONT_24.render(input_text, True, WHITE)
+                screen.blit(text, (SCREEN_WIDTH // 2 - 180, SCREEN_HEIGHT // 2 - 15))
+            text = FONT_24.render('Имя', True, WHITE)
+            screen.blit(text, (SCREEN_WIDTH // 2 - 220, SCREEN_HEIGHT // 2 - 180))
+            text = FONT_24.render('Пароль', True, WHITE)
+            screen.blit(text, (SCREEN_WIDTH // 2 - 220, SCREEN_HEIGHT // 2 - 80))
+            text = FONT_38.render('Комментарии', True, WHITE)
+            screen.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2, 50))
+            self.update_commenting(screen)
             pygame.display.update()
