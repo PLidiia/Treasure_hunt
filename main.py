1492ef43200efbf4d483533d3ca8bd6d03433938
PLidiia <151846713+PLidiia@users.noreply.github.com>
2023-12-30 11:42:34 +0700

    Добавление блоков у меню

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 from Constants import *
 from Entrance import Enter
 
+
 if __name__ == "__main__":
     entry = Enter()
+    entry.append_block_menu('Имя')
+    entry.append_block_menu('Пароль')
+    entry.append_block_menu('Настройки')
     entry.run()
