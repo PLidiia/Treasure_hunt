972a023799181b04b25bd6b3ed62dd2009bd1d38
PLidiia <151846713+PLidiia@users.noreply.github.com>
2024-01-01 17:03:04 +0700

    Создание дизайна

---
+++ images/tiles/chest_for_hp_or_speed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="chest_for_hp_or_speed" tilewidth="64" tileheight="64" tilecount="1248" columns="52">
+ <image source="../terrain/med_thing.png" width="3368" height="1550"/>
+</tileset>
